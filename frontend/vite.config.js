522213b54ea27300708d6faef7e300629d67c2e0
DeathStroke <149133004+surajgoswami11@users.noreply.github.com>
2025-09-16 10:35:20 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import vercel from "vite-plugin-vercel"
+
+
 
 export default defineConfig({
+  server:{
+    port:'https://ghup-shup.onrender.com'},
   plugins: [react(), tailwindcss()],
 });
