3671f73c82b5aa9677ffee3928671f18dc5d7e72
surajgoswami11 <oodinsonthor11@gmail.com>
2025-09-16 23:55:18 +0530

    new changes

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://ghup-shup.onrender.com",
     credentials: true,
   })
 );
