02d31a437163e1cb8675900c22eba037a6728874
surajgoswami11 <oodinsonthor11@gmail.com>
2025-09-16 23:32:09 +0530

    new push

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import vercel from "vite-plugin-vercel"
-
-
 
 export default defineConfig({
-  server:{
-    port:'https://ghup-shup.onrender.com'},
+  server: {
+    port: "https://ghup-shup.onrender.com",
+  },
   plugins: [react(), tailwindcss()],
 });
